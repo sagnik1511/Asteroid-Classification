5506559dd29e1793792a4f502ab77000ce30f7af
Sagnik Roy <sagnik.jal00@gmail.com>
2022-03-23 21:39:25 +0530

    removed unnecessary files

--- test.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_selection import VarianceThreshold
-import pandas as pd
-
-df = pd.read_csv("data/primary/test.csv", usecols = ["id", "absolute_magnitude"])
-scaler = VarianceThreshold(0.9)
-
-op = scaler.fit_transform(df)
-print(op)
