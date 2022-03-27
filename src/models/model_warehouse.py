3d2f6c88d7492c58a0167c4401ae77090814c92d
Sagnik Roy <sagnik.jal00@gmail.com>
2022-03-27 11:35:32 +0530

    added config for xgb model

--- src/models/model_warehouse.py
+++ src/models/model_warehouse.py
@@ -1,6 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 
 warehouse_dict = {
     "RandomForestClassifier": RandomForestClassifier,
+    "XGBoostClassifier": XGBClassifier,
 }
