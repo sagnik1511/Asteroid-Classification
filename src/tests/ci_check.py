813eb4c6e4dd8fa73362bacc2412c189df377750
Sagnik Roy <sagnik.jal00@gmail.com>
2022-06-14 14:46:30 +0530

    maybe last test

--- src/tests/ci_check.py
+++ src/tests/ci_check.py
@@ -3,8 +3,8 @@ from src.models.custom_classifier_training import classifier_training
 
 if __name__ == "__main__":
 
-    processing_config = "config\\models\\custom_training_config.yaml"
-    model_config = "config\\models\\tree_based_model.yaml"
+    processing_config = "config/models/custom_training_config.yaml"
+    model_config = "config/models/tree_based_model.yaml"
 
     # training classifier
     classifier_training(processing_config, model_config, testing=True)
